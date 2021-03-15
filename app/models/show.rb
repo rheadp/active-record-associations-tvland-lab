67d4e03565e5bf5fae0c0977d4e69e07f50f2f9b
rheadp <rheadpea@msu.edu>
2021-03-15 18:07:34 -0400

    commit tree b981f301adfbe580ae06dc1a8104f4c48002f280

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+  def actors_list
+    self.characters.map{|c| Actor.find(c.actor_id).full_name}
+  end
+
 end
